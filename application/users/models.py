c72df5bfb6ecebce77b44a3098621e794ca5f2c3
Roman Ivanov <sitdoff@gmail.com>
2024-09-09 11:27:38 +0300

    update UserModel

--- application/users/models.py
+++ application/users/models.py
@@ -1,9 +1,10 @@
+from flask_login import UserMixin
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..db import db
 
 
-class UserModel(db.Model):
+class UserModel(db.Model, UserMixin):
     __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     email: Mapped[str] = mapped_column(unique=True)
