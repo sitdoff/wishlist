915dde8c42771f1450c36937679826f6096c9b0b
Roman Ivanov <sitdoff@gmail.com>
2024-09-10 15:44:29 +0300

    disable login_message

--- application/__init__.py
+++ application/__init__.py
@@ -27,6 +27,7 @@ def create_app():
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = "users.login"
+    login_manager.login_message = None
 
     @login_manager.user_loader
     def load_user(user_id):
