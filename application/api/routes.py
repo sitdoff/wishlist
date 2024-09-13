cc705601d1b310ab478c2d26126951ccc9749801
Roman Ivanov <sitdoff@gmail.com>
2024-09-13 09:54:21 +0300

    add api all_items route

--- application/api/routes.py
+++ application/api/routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request
+from flask_jwt_extended import (create_access_token, get_jwt_identity,
+                                jwt_required)
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -43,8 +45,36 @@ def login():
 
 
 @bp.route("items/", methods=["GET", "POST"])
+@jwt_required()
 def all_items():
-    pass
+
+    if request.method == "POST":
+        required_fields = ("name", "url", "price", "description")
+
+        item_data = request.get_json()
+        if not item_data:
+            return {"error": "Item data is required"}, 400
+
+        missing_fields = [field for field in required_fields if field not in item_data]
+        if missing_fields:
+            return {
+                "error": f"Missing required fields: {', '.join(missing_fields)}"
+            }, 400
+
+        empty_fields = [field for field in item_data if not item_data[field]]
+        if empty_fields:
+            return {"error": f"Empty fields: {', '.join(empty_fields)}"}, 400
+
+        try:
+            user = UserModel.query.filter_by(email=get_jwt_identity()).first()
+            item = ItemModel(**item_data, user_id=user.id)
+            db.session.add(item)
+            db.session.commit()
+            return {"success": "Item created", "item": item.to_dict()}, 201
+        except Exception as e:
+            return {"error": str(e)}, 400
+    items = UserModel.query.filter_by(email=get_jwt_identity()).first().items
+    return [item.to_dict() for item in items]
 
 
 @bp.route("items/<int:item_id>/", methods=["GET", "PATCH", "DELETE"])
